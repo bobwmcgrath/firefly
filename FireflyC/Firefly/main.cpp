533880067015ee5cae88134545878891397e6eb3
bob mcgrath <bobwmcgrath@gmail.com>
2019-04-05 01:14:00 -0500

    while loops

--- FireflyC/Firefly/main.cpp
+++ FireflyC/Firefly/main.cpp
@@ -8,39 +8,46 @@ int carrier = 1;  // a variable that is 1 if the ADC reading is higher than the 
 int lastCarrier = 1; // the previous carrier value for comparison
 int carrierCounter = 1; // number of times in a row that the number of lumens reversed direction i.e.  higher or lower
 int blinkCounter = 1; // number of times through the bloop loop, on and off
-int senseCounter = 1; // number of times through the sense loop
+int senseCounter1 = 1; // number of times through the sense loop
+int senseCounter2 =1; // becuse I cant do floats so this increments one for every 10 senseCounter1s 
 
 int setup(void){
-	DDRB |= _BV(DDB2); // set as output
+	//DDRB |= _BV(DDB2); // set as output
+	DDRB = 0b0101;
 }
 
 
 int bloop (void) {
 PORTB |= _BV(PORTB2);
 PORTB |= _BV(PORTB0);// turn on
-_delay_ms(50);
+_delay_ms(10);
 
 PORTB &= ~_BV(PORTB2);
 PORTB &= ~_BV(PORTB0); // turn off
-_delay_ms(50);
+_delay_ms(10);
 
-
+blinkCounter +=1;
+return blinkCounter;
 }
 
-void shine (void) {	
-	blinkCounter +=1;
-	if (blinkCounter <50) { //run loop for a number of times
-		bloop(); // blink
-		shine(); // restart blinking loop
-	}
-	else {blinkCounter = 0; senseCounter = 0;
-	carrierCounter=0;} // set counters back to zero and restart main loop
+//int shine (void) {	
+	//_delay_ms(5000);
+	//bloop();
+	//bloop();
+	//blinkCounter +=1;
+	//if (blinkCounter <50) { //run loop for a number of times
+		//bloop(); // blink
+		//shine(); // restart blinking loop
+	//}
+	//else 
+	//{blinkCounter = 0; senseCounter = 0;
+	//carrierCounter=0;} // set counters back to zero and restart main loop
+	//return blinkCounter;
 	
-}
+//}
 
-int sense (void) {
-_delay_ms(50);
-
+void sense () {
+_delay_ms(10);
 lastLumens = lumens;
 lastCarrier=carrier;
 
@@ -50,18 +57,28 @@ ADCSRA = ADCSRA | 1<<ADSC;     // Start
 while (ADCSRA & 1<<ADSC);      // Wait while conversion in progress
 lumens = ADCL;               // Copy result to lumens
 
-senseCounter += 1;
+senseCounter1 += 1;
+if (senseCounter1 % 10 == 0) senseCounter2 += 1;
 if (lumens > lastLumens) carrier = 1;
 else carrier = 0;
-if (carrier != lastCarrier) carrierCounter+=1;
+if (carrier != lastCarrier) carrierCounter += 1;
 else carrierCounter=0;
-if (carrierCounter==5) senseCounter += 10; // this is the multiplier that modulates the phase variance
-if (senseCounter<100) sense(); //why does everything break for values over 11
+if (carrierCounter==5) senseCounter1 += senseCounter2; // this is the multiplier that modulates the phase variance
+//return senseCounter1;
 }
 
 int loop (void) {
-	sense();
-	shine();
+	//sense();
+	while (senseCounter1<100) sense(); 
+	//shine();
+	while(blinkCounter <50) bloop(); // blink
+		//shine(); // restart blinking loop
+		//}
+		//else
+		blinkCounter = 0; 
+		senseCounter1 = 0;
+		carrierCounter=0;
+		senseCounter2=0;
 
 }
 
